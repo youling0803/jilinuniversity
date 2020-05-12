0ad23896f9b3bca6b6bd80f9823471131e2e54cc
Xuke <Xuke@DESKTOP-536VODM>
2020-05-12 14:58:47 +0800

    模板页书写

--- JilinUniversity/user/TopicManage.aspx.cs
+++ JilinUniversity/user/TopicManage.aspx.cs
@@ -10,6 +10,7 @@ namespace JilinUniversity.user
 {
     public partial class TopicManage : System.Web.UI.Page
     {
+        //页面加载
         protected void Page_Load(object sender, EventArgs e)
         {
             DataTable dt = new DataTable();
@@ -32,34 +33,56 @@ namespace JilinUniversity.user
             GridViewDetection.DataBind();
         }
 
+        //新增点击事件
         protected void NewAddAntiDetection(object sender, EventArgs e)
         {
 
         }
 
+        //模态框中确定按钮事件
         protected void GetReason_Invalid(object sender, EventArgs e)
         {
 
         }
 
+        //GirdView操作点击事件
         protected void LBdownload_Command(object sender, CommandEventArgs e)
         {
             
         }
 
+        //GridView数据绑定事件
+        //用于处理分页为1时显示分页的问题
         protected void GridViewDetection_DataBound(object sender, EventArgs e)
         {
-
+            if (GridViewDetection.Rows.Count != 0)
+            {
+                Control table = GridViewDetection.Controls[0];
+                int count = table.Controls.Count;
+                table.Controls[count - 1].Visible = true;
+            }
         }
 
+        //GridView行数据绑定事件
+        //用于操作中按键的显示与隐藏
         protected void GridViewDetection_RowDataBound(object sender, GridViewRowEventArgs e)
         {
 
         }
 
+        //GridView分页点击事件
+        //用于操作GirdView的换页
         protected void GridViewDetection_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
-
+            if (e.NewPageIndex < 0)
+            {
+                GridViewDetection.PageIndex = 0;
+            }
+            else
+            {
+                GridViewDetection.PageIndex = e.NewPageIndex;
+            }
+            //GridViewBind();
         }
     }
 }
