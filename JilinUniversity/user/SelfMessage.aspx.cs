09b1a61ce142cc4a01a1db7b0f21eb9a9aa18509
Xuke <Xuke@DESKTOP-536VODM>
2020-05-13 17:15:56 +0800

    添加样式

---
+++ JilinUniversity/user/SelfMessage.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JilinUniversity.user
+{
+    public partial class SelfMessage : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GetReason_Invalid(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
